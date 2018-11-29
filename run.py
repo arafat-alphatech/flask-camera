f630c704afdfa6c5f7877aa4eaa0d74b588a5977
arafat <arafat@alphatech.id>
2018-11-29 10:41:30 +0700

    comment imshow

--- run.py
+++ run.py
@@ -40,7 +40,7 @@ def grading(data_uri):
 	gray = cv2.bilateralFilter(gray, 11, 17, 17)
 	#find the edges
 	edged = cv2.Canny(gray, 85, 250)
-	cv2.imshow('edged', edged)
+	# cv2.imshow('edged', edged)
 	#find the contours
 	temp_img, contours, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -95,7 +95,7 @@ def grading(data_uri):
 		#warp persepctive
 		paper = cv2.warpPerspective(original_image, M, (425, 550))
 		answers, paper, codes = ProcessPage(paper)
-		cv2.imshow("Scanned Paper", paper)
+		# cv2.imshow("Scanned Paper", paper)
 
 	#draw the contour
 	if biggestContour is not None:
@@ -107,8 +107,8 @@ def grading(data_uri):
 		else:
 			cv2.drawContours(image, [biggestContour], -1, (0, 0, 255), 3)
 
-	cv2.imshow("Original Image", cv2.resize(image, (0, 0), fx=0.7, fy=0.7))
-	cv2.waitKey(0)
+	# cv2.imshow("Original Image", cv2.resize(image, (0, 0), fx=0.7, fy=0.7))
+	# cv2.waitKey(0)
 	return codes, answers
 
 
